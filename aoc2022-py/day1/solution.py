4ee1487c235324139c3cbc5cf531a6d61dcb7a4b
Prashun Chitrakar <prashunchi@gmail.com>
2022-12-01 18:22:30 +0545

    Remove unnecessary code

--- aoc2022-py/day1/solution.py
+++ aoc2022-py/day1/solution.py
@@ -4,9 +4,6 @@ import pathlib
 
 def main():
     top = sorted(reduce(lambda a, b: [*a, 0] if b == -1 else [*a[:-1], a[-1] + b], [int(line) if line != '\n' else -
-                 1 for line in open(pathlib.Path(__file__).parent.absolute() / "input.txt", 'r')], [0]), reverse=True)[0]
+                 1 for line in open(pathlib.Path(__file__).parent.absolute() / "input.txt", 'r')], [0]), reverse=True)
 
-    sum_top_3 = sum(sorted(reduce(lambda a, b: [*a, 0] if b == -1 else [*a[:-1], a[-1] + b], [int(
-        line) if line != '\n' else -1 for line in open(pathlib.Path(__file__).parent.absolute() / "input.txt", 'r')], [0]), reverse=True)[:3])
-
-    print(top, sum_top_3)
+    print(top[0], sum(top[:3]))
