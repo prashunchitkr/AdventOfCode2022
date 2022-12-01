585a07565b22c29404349c2e50d283d064ad620e
Prashun Chitrakar <prashunchi@gmail.com>
2022-12-01 18:36:46 +0545

    Remove reduce method

--- aoc2022-py/day1/solution.py
+++ aoc2022-py/day1/solution.py
@@ -1,9 +1,9 @@
-from functools import reduce
 import pathlib
 
 
 def main():
-    top = sorted(reduce(lambda a, b: [*a, 0] if b == -1 else [*a[:-1], a[-1] + b], [int(line) if line != '\n' else -
-                 1 for line in open(pathlib.Path(__file__).parent.absolute() / "input.txt", 'r')], [0]), reverse=True)
+    parsed = sorted([sum(map(lambda x: int(x) if x != '' else 0, s.split('\n'))) for s in open(pathlib.Path(__file__).parent.absolute() / "input.txt",
+                                                                                               'r').read().split("\n\n")], reverse=True)
 
-    print(top[0], sum(top[:3]))
+    print(parsed[0])
+    print(sum(parsed[:3]))
