0bd80e73e672bc2d83f61d582b6b64c2dede8355
Prashun Chitrakar <prashunchi@gmail.com>
2022-12-06 18:52:01 +0545

    Refactor main method to call function dynamically

--- src/main.py
+++ src/main.py
@@ -5,9 +5,15 @@ from day4 import Day4
 from day5 import Day5
 from day6 import Day6
 
-# day1()
-# Day2()
-# Day3()
-# Day4()
-# Day5()
-Day6()
+print("Enter day number for solution: ")
+day = input()
+
+if not day.isnumeric() or int(day) < 1 or int(day) > 6:
+    print("Invalid day number")
+    exit(1)
+
+method_name = f"Day{int(day)}"
+possibles = globals().copy()
+possibles.update(locals())
+method = possibles.get(method_name)
+method()
